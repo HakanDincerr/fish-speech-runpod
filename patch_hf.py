287300e2ed44b61c0ef19963961c198c27823da0
HakanDincerr <127485038+HakanDincerr@users.noreply.github.com>
2026-04-26 00:39:15 +0300

    Update patch_hf.py

--- patch_hf.py
+++ patch_hf.py
@@ -1,6 +1,5 @@
 import os
 import re
-import sys
 
 site = '/usr/local/lib/python3.11/dist-packages'
 
@@ -25,39 +24,109 @@ with open(hf_path, 'w') as f:
     f.write(patched)
 print('1. hf.py patched')
 
-# 2. torchvision'i uninstall et, stub koy
+# 2. torchvision transforms stub - tam liste
+tv_transforms = f'{site}/torchvision/transforms/__init__.py'
+if os.path.exists(tv_transforms):
+    with open(tv_transforms, 'w') as f:
+        f.write('''# torchvision transforms stub
+class InterpolationMode:
+    BICUBIC = "bicubic"
+    BILINEAR = "bilinear"
+    NEAREST = "nearest"
+    LANCZOS = "lanczos"
+    NEAREST_EXACT = "nearest_exact"
+
+class AutoAugmentPolicy:
+    CIFAR10 = "cifar10"
+    IMAGENET = "imagenet"
+    SVHN = "svhn"
+
+class AutoAugment:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class RandAugment:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class TrivialAugmentWide:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class AugMix:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class Compose:
+    def __init__(self, transforms): self.transforms = transforms
+    def __call__(self, x):
+        for t in self.transforms: x = t(x)
+        return x
+
+class ToTensor:
+    def __call__(self, x): return x
+
+class Normalize:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class Resize:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class CenterCrop:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class RandomCrop:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class RandomHorizontalFlip:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+class RandomVerticalFlip:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x): return x
+
+def pil_to_tensor(x): return x
+
+class functional:
+    @staticmethod
+    def pil_to_tensor(x): return x
+    @staticmethod
+    def to_tensor(x): return x
+    @staticmethod
+    def normalize(x, *args, **kwargs): return x
+''')
+    print('2. torchvision.transforms stubbed')
+
+# 3. torchvision __init__ stub
 tv_init = f'{site}/torchvision/__init__.py'
 if os.path.exists(tv_init):
-    stub_tv = '''# torchvision stub - binary uyumsuzlugu nedeniyle devre disi
+    with open(tv_init, 'w') as f:
+        f.write('''# torchvision stub
+from torchvision import transforms
+
 class _Stub:
     def __getattr__(self, name):
-        raise ImportError(f"torchvision.{name} not available (stub mode)")
+        return _Stub()
+    def __call__(self, *args, **kwargs):
+        return _Stub()
 
 datasets = _Stub()
 io = _Stub()
 models = _Stub()
 ops = _Stub()
-transforms = _Stub()
 utils = _Stub()
 
-class _HAS_OPS:
-    pass
-
 def __getattr__(name):
-    raise ImportError(f"torchvision.{name} not available (stub mode)")
-'''
-    with open(tv_init, 'w') as f:
-        f.write(stub_tv)
-    print('2. torchvision stubbed')
+    return _Stub()
+''')
+    print('3. torchvision.__init__ stubbed')
 
-# 3. torchvision.transforms stub
-tv_transforms = f'{site}/torchvision/transforms/__init__.py'
-if os.path.exists(tv_transforms):
-    with open(tv_transforms, 'w') as f:
-        f.write('class InterpolationMode:\n    BICUBIC = "bicubic"\n    BILINEAR = "bilinear"\n    NEAREST = "nearest"\n\ndef functional():\n    pass\n')
-    print('3. torchvision.transforms stubbed')
-
-# 4. transformers image_utils - torchvision import'u bos gecir
+# 4. transformers image_utils - torchvision import devre disi
 img_utils = f'{site}/transformers/image_utils.py'
 if os.path.exists(img_utils):
     with open(img_utils) as f:
