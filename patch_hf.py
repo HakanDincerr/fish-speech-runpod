3236905519833058ea8643e50d264f6d7c5489d5
HakanDincerr <127485038+HakanDincerr@users.noreply.github.com>
2026-04-26 00:12:37 +0300

    Update patch_hf.py

--- patch_hf.py
+++ patch_hf.py
@@ -1,51 +1,73 @@
 import os
+import re
+import sys
 
-# 1. hf.py patch - no_init_weights
-hf_path = '/usr/local/lib/python3.11/dist-packages/sglang_omni/utils/hf.py'
+site = '/usr/local/lib/python3.11/dist-packages'
+
+# 1. hf.py patch
+hf_path = f'{site}/sglang_omni/utils/hf.py'
 with open(hf_path) as f:
     content = f.read()
 
-import re
-stub = ('from contextlib import contextmanager\n\n'
-        '@contextmanager\n'
-        'def no_init_weights(_enable=True):\n'
-        '    yield\n\n')
-
+stub = (
+    'from contextlib import contextmanager\n\n'
+    '@contextmanager\n'
+    'def no_init_weights(_enable=True):\n'
+    '    yield\n\n'
+)
 patched = re.sub(
     r'try:\s*\n\s*from transformers[^\n]*no_init_weights[^\n]*\n.*?except ImportError:\s*\n\s*from transformers[^\n]*no_init_weights',
     stub.strip(),
     content,
     flags=re.DOTALL
 )
-
 with open(hf_path, 'w') as f:
     f.write(patched)
-print('hf.py patched!')
+print('1. hf.py patched')
 
-# 2. torchvision _meta_registrations.py patch
-# register_fake cagrisini devre disi birak
-tv_meta_path = '/usr/local/lib/python3.11/dist-packages/torchvision/_meta_registrations.py'
-if os.path.exists(tv_meta_path):
-    with open(tv_meta_path) as f:
-        tv_content = f.read()
-    # register_fake cagrilarini no-op yap
-    tv_patched = tv_content.replace(
-        '@torch.library.register_fake("torchvision::nms")',
-        '# @torch.library.register_fake("torchvision::nms")\nif False:'
-    ).replace(
-        '@torch.library.register_fake("torchvision::roi_align")',
-        '# @torch.library.register_fake("torchvision::roi_align")\nif False:'
-    ).replace(
-        '@torch.library.register_fake("torchvision::roi_pool")',
-        '# @torch.library.register_fake("torchvision::roi_pool")\nif False:'
-    ).replace(
-        '@torch.library.register_fake("torchvision::_new_empty_tensor_op")',
-        '# @torch.library.register_fake("torchvision::_new_empty_tensor_op")\nif False:'
+# 2. torchvision'i uninstall et, stub koy
+tv_init = f'{site}/torchvision/__init__.py'
+if os.path.exists(tv_init):
+    stub_tv = '''# torchvision stub - binary uyumsuzlugu nedeniyle devre disi
+class _Stub:
+    def __getattr__(self, name):
+        raise ImportError(f"torchvision.{name} not available (stub mode)")
+
+datasets = _Stub()
+io = _Stub()
+models = _Stub()
+ops = _Stub()
+transforms = _Stub()
+utils = _Stub()
+
+class _HAS_OPS:
+    pass
+
+def __getattr__(name):
+    raise ImportError(f"torchvision.{name} not available (stub mode)")
+'''
+    with open(tv_init, 'w') as f:
+        f.write(stub_tv)
+    print('2. torchvision stubbed')
+
+# 3. torchvision.transforms stub
+tv_transforms = f'{site}/torchvision/transforms/__init__.py'
+if os.path.exists(tv_transforms):
+    with open(tv_transforms, 'w') as f:
+        f.write('class InterpolationMode:\n    BICUBIC = "bicubic"\n    BILINEAR = "bilinear"\n    NEAREST = "nearest"\n\ndef functional():\n    pass\n')
+    print('3. torchvision.transforms stubbed')
+
+# 4. transformers image_utils - torchvision import'u bos gecir
+img_utils = f'{site}/transformers/image_utils.py'
+if os.path.exists(img_utils):
+    with open(img_utils) as f:
+        content = f.read()
+    patched = content.replace(
+        'if is_torchvision_available():',
+        'if False:  # torchvision disabled'
     )
-    with open(tv_meta_path, 'w') as f:
-        f.write(tv_patched)
-    print('torchvision _meta_registrations.py patched!')
-else:
-    print('torchvision _meta_registrations.py not found, skipping')
+    with open(img_utils, 'w') as f:
+        f.write(patched)
+    print('4. transformers/image_utils.py patched')
 
-print('All patches applied!')
+print('All patches done!')
